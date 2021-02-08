8395332ef1414760581825d31bf8795bfaf916b1
SteffenKloempges <steffen.kloempges@gmail.com>
2021-02-08 16:06:36 +0100

    done

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+let size = document.getElementById("element");
+
+function small() {
+    size.className = " ";
+    size.classList = ("klein");
+}
+
+function middle() {
+    size.className = " ";
+    size.classList = ("normal");
+}
+
+function big() {
+    size.className = " ";
+    size.classList = ("bigger");
+}
